01a19f9106a8673ce459db257faed7127cef1ceb
Kuo <kliang3@student.wgu.edu>
2022-01-17 21:50:56 +0000

    add resource bundle fr

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
+
+    ResourceBundle rb = ResourceBundle.getBundle("source/Nat", Locale.getDefault());
 
     @Override
     public void start(Stage primaryStage) throws Exception{
