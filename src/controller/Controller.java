b4d025b03f247a8c8b83367addd56b67cccb3894
Kuo <kliang3@student.wgu.edu>
2022-01-17 16:57:26 +0000

    add login view

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -9,7 +9,8 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        //
+        //TODO write code here.
+
 
     }
 }
---
+++ src/controller/LoginViewController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginViewController implements Initializable {
+    public Label loginLabel;
+    public Label passwordLabel;
+    public TextField loginTxtField;
+    public TextField passwordTxtField;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
